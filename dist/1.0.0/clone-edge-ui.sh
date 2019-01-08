c640cbb53e0a8c11d67138af5806e0905c4c6bdf
root <root@apigee-internal-idp-service.c.edge-apigee.internal>
2019-01-08 05:04:55 +0000

    edge classic ui port

--- dist/1.0.0/clone-edge-ui.sh
+++ dist/1.0.0/clone-edge-ui.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 APIGEE_ROOT=/opt/apigee
-EDGE_UI_CLONE_FIX="https://raw.github.com/rajeshm7910/apigee-internal-idp/tree/master/dist/1.0.0/edge-classic-ui-clone.tar.gz"
-#EDGE_UI_CLONE="http://localhost:88/customrepo/edge-classic-ui-clone.tar.gz"
+#EDGE_UI_CLONE_FIX="https://raw.github.com/rajeshm7910/apigee-internal-idp/tree/master/dist/1.0.0/edge-classic-ui-clone.tar.gz"
+EDGE_UI_CLONE="http://localhost:88/customrepo/edge-classic-ui-clone.tar.gz"
+PORT=${1:-9098}
 
+echo "Setting edge-classic ui on port : " $PORT
 source $APIGEE_ROOT/etc/defaults.sh
 if [[ ! -d "/opt/apigee/edge-ui" ]] ; then
         echo "Edge  UI not installed in this server"
@@ -15,3 +17,4 @@ su -m -c "cp -fr $APIGEE_ROOT/edge-ui/* $APIGEE_ROOT/edge-classic-ui-4.18.05-0.0
 su -m -c "ln -s $APIGEE_ROOT/edge-classic-ui-4.18.05-0.0.0 $APIGEE_ROOT/edge-classic-ui" apigee
 
 su -m -c "curl -L $EDGE_UI_CLONE | tar xz  -C  /opt/apigee/edge-classic-ui/" apigee
+su -m -c "sed -i.bak s/9098/$PORT/g /opt/apigee/edge-classic-ui/lib/settings" apigee
